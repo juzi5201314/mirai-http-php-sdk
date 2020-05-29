9c66957bc67b1ab0de338cde0d4839089349ce8b
soeur <juzi201314@gmail.com>
2020-05-29 21:16:32 +0800

    为ApiError添加kind

--- src/ApiError.php
+++ src/ApiError.php
@@ -7,8 +7,15 @@ use Throwable;
 
 class ApiError extends \Exception {
 
-    public function __construct($message, $code = 0, Throwable $previous = null) {
-        parent::__construct($message, $code, $previous);
+    //api调用过程中产生的其他错误。如http错误，json解析错误等
+    const KIND_OTHER = -1;
+
+    // 除了other，其他与mirai http api的错误代码相同
+    public int $kind;
+
+    public function __construct(string $message, int $kind = -1) {
+        parent::__construct($message, 0, null);
+        $this->kind = $kind;
     }
 
 }
